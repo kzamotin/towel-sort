cb8a178eba7d009e8183c5233ea894130dc6bdd4
KRYPTONITE\k.zamotin <k.zamotin@kryptonite.ru>
2024-08-05 13:56:15 +0300

    feat: sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,23 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+
+var result= []
+
+    var cnt=0;
+    for (var i in matrix) {
+        var tmp=[]
+    if (cnt %2 ==0){
+        tmp = matrix.at(i);
+        }
+    else {
+        tmp = matrix.at(i).reverse();
+    }
+    cnt ++;
+    
+    for( var j in tmp) {
+        result.push(tmp.at(j));
+    }
+}
+  return result;
 }
